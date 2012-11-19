ba70b835f0ead8277a760abee4e75ec7fb1e8b65
Caleb Wang <cw@berkeley.edu>
2012-11-18 18:34:17 -0800

    some server work

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import bottle
+import urllib
+import facebook
 
 
 app_id = '409886029080423'
@@ -20,6 +22,14 @@ def login():
 
 @app.route('/callback')
 def callback():
-    args = dict(client_id=FACEBOOK_APP_ID, redirect_uri=self.request.path_url)
+    args = dict(client_id=app_id, redirect_uri='http://localhost:8080/callback')
     args['client_secret'] = app_secret
+    args['code'] = bottle.request.query.code
+    reponse = facebook.cgi.parse_qs(urllib.urlopen(
+        "https://graph.facebook.com/oauth/access_token?" +
+            urllib.urlencode(args)).read())
+    access_token = response["access_token"][-1]
     return "hi"
+
+if __name__ == '__main__':
+    app.run(host = 'localhost', port = '8080', debug = True)
